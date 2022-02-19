1ab437a714ec931b8d7510b2e45e731d9832431a
Sergey_Fokin <momomogggq@gmail.com>
2022-02-19 14:35:26 +0400

    deploy

---
+++ precache-manifest.a05a2d9ffa4a21b0362ea430d7e1ab8a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c8d511c48d3bd4138d",
+    "url": "/weather-vue/css/about.8c845adb.css"
+  },
+  {
+    "revision": "c98e0c349a0e16aa9c69",
+    "url": "/weather-vue/css/app.172e63f0.css"
+  },
+  {
+    "revision": "565311eb63ca33bfa29487d8cca22744",
+    "url": "/weather-vue/img/bluesky.565311eb.jpeg"
+  },
+  {
+    "revision": "d11918bba89d78f4ef95a2011b59f5b3",
+    "url": "/weather-vue/img/cloud.d11918bb.svg"
+  },
+  {
+    "revision": "64b3a9c40c56bdbbbab10ed0625bbec4",
+    "url": "/weather-vue/img/humidity.64b3a9c4.svg"
+  },
+  {
+    "revision": "6ce3d3b5aa07340b71b8a3fb028a1725",
+    "url": "/weather-vue/img/logo.6ce3d3b5.png"
+  },
+  {
+    "revision": "663dd00fde215bc4e659373f4ae533d4",
+    "url": "/weather-vue/img/pressure.663dd00f.svg"
+  },
+  {
+    "revision": "17698e44684719922087eca18c0d0295",
+    "url": "/weather-vue/img/wind.17698e44.svg"
+  },
+  {
+    "revision": "9d226422c040dd6ab5d1730e48875c39",
+    "url": "/weather-vue/index.html"
+  },
+  {
+    "revision": "41c8d511c48d3bd4138d",
+    "url": "/weather-vue/js/about.c0c8ab0c.js"
+  },
+  {
+    "revision": "c98e0c349a0e16aa9c69",
+    "url": "/weather-vue/js/app.688c5d63.js"
+  },
+  {
+    "revision": "c2520c17b2149d313eef",
+    "url": "/weather-vue/js/chunk-vendors.527c3999.js"
+  },
+  {
+    "revision": "b5621ad93eb18b751cfad308347ecc96",
+    "url": "/weather-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weather-vue/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/weather-vue/precache-manifest.a05a2d9ffa4a21b0362ea430d7e1ab8a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "weather-vue"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
